d4f536918d3d0fe7b83a99ec91266c4404471bda
feedFloward <flo.cnrd@gmail.com>
2020-07-14 02:11:40 +0200

    backend restructure with answer dictionary

--- backend/handling.py
+++ backend/handling.py
@@ -2,9 +2,13 @@ from .json_parser import JsonParser
 from .models.classification import select_model
 import json
 
+#wieder rausnehmen, wenn answer_dict neu strukturiert wurde:
+from .models.classification import answer_dict
+
 def handle_request(request_body):
     request_dict = json.loads(request_body)
     parsed_body = JsonParser(**request_dict)
     model = select_model(**parsed_body)
     model.fit()
-    return json.dumps({'Z':model.predict_inputspace().tolist()})
+    model.predict_inputspace()
+    return json.dumps(answer_dict.response)
